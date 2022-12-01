48297758ee537a3ec84374979fcae4088476ea1e
Gaurav12342 <gauravpendharkar2@gmail.com>
2022-12-02 00:02:09 +0530

    Added test with skip

--- src/pages/auth/sign-in/Login.test.tsx
+++ src/pages/auth/sign-in/Login.test.tsx
@@ -2,7 +2,7 @@ import { screen, render } from '@testing-library/react';
 import Login from './Login';
 
 describe("Login Component", () => {
-    test("login form image", () => {
+    test.only("login form image", () => {
         render(<Login />);
         const imageElement = screen.getByAltText("desk-image");
         expect(imageElement).toBeInTheDocument();
